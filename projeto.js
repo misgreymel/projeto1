557d77557d9202b5dc383a4b03410f8f20dcccde
misgreymel <141650128+misgreymel@users.noreply.github.com>
2023-09-11 08:40:19 -0400

    projeto.js

---
+++ projeto.js
@@ -0,0 +1,33 @@
+
+    let currentImageIndex = 0;
+    const images = ['/imagens visita/todes.jpg', '/imagens visita/nosdecima.jpg', '/imagens visita/pesamoo.jpg', '/imagens visita/porcoaut.jpg','/imagens visita/teris.jpg', '/imagens visita/nosnoonibus.jpg','/imagens visita/kauatras.jpg','/imagens visita/euebella1.jpg', '/imagens visita/euebella2.jpg','/imagens visita/euebella3.jpg','/imagens visita/bellalouca.jpg'];
+
+    function openModal(imageSrc) {
+        const modal = document.getElementById('myModal');
+        const modalImage = document.getElementById('modalImage');
+
+        currentImageIndex = images.indexOf(imageSrc);
+
+        modal.style.display = 'block';
+        modalImage.src = imageSrc;
+    }
+
+    function closeModal() {
+        const modal = document.getElementById('myModal');
+        modal.style.display = 'none';
+    }
+
+    function nextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        const modalImage = document.getElementById('modalImage');
+        modalImage.src = images[currentImageIndex];
+    }
+
+    // Fecha o modal ao clicar fora da imagem
+    const modal = document.getElementById('myModal');
+    modal.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
