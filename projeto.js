4af7d66d4e5da8d55d97af2fb3c427c79a22c684
misgreymel <141650128+misgreymel@users.noreply.github.com>
2023-09-11 10:48:17 -0400

    projeto.js

--- projeto.js
+++ projeto.js
@@ -1,6 +1,6 @@
 
     let currentImageIndex = 0;
-    const images = ['/imagens visita/todes.jpg', '/imagens visita/nosdecima.jpg', '/imagens visita/pesamoo.jpg', '/imagens visita/porcoaut.jpg','/imagens visita/teris.jpg', '/imagens visita/nosnoonibus.jpg','/imagens visita/kauatras.jpg','/imagens visita/euebella1.jpg', '/imagens visita/euebella2.jpg','/imagens visita/euebella3.jpg','/imagens visita/bellalouca.jpg'];
+    const images = ['imagens visita/todes.jpg', 'imagens visita/nosdecima.jpg', 'imagens visita/pesamoo.jpg', 'imagens visita/porcoaut.jpg','imagens visita/teris.jpg', 'imagens visita/nosnoonibus.jpg','imagens visita/kauatras.jpg','imagens visita/euebella1.jpg', 'imagens visita/euebella2.jpg','imagens visita/euebella3.jpg','imagens visita/bellalouca.jpg'];
 
     function openModal(imageSrc) {
         const modal = document.getElementById('myModal');
